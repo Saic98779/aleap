46574ca8779f63e071d38477bc25ff54852278f3
saic98 <134575105+saic98@users.noreply.github.com>
2026-07-19 11:35:37 +0530

    Include formstage enum

--- src/main/java/com/metaverse/workflow/startupSurvey/Enums/FormStage.java
+++ src/main/java/com/metaverse/workflow/startupSurvey/Enums/FormStage.java
@@ -1,10 +1,6 @@
 package com.metaverse.workflow.startupSurvey.Enums;
 
 public enum FormStage {
-    BASIC_INFORMATION,
-    BUSINESS_OVERVIEW,
-    TEAM_AND_RESOURCE,
-    MARKET_AND_CUSTOMERS,
-    TECHNOLOGY_AND_INNOVATION
+   COMPLETED, PENDING
 }
 
